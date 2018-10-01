803bb22d09e488a79f48fdce7ca0f84c1478d023
Gavin <4659489@qq.com>
2018-10-01 18:38:48 +0800

    列表页加链接方法

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 
 from . import views
-
+app_name='blog'
 urlpatterns = [
     url('^$',views.index),
-    url('^article/(?P<article_id>[0-9]+)$',views.article_page),
+    url('^article/(?P<article_id>[0-9]+)$',views.article_page,name='article_page'),
 ]
