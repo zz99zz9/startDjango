561c38961ad20263b6c37d98d5b3447954d16a1f
Gavin <4659489@qq.com>
2018-10-01 22:00:56 +0800

    高级admin操作及防卡变量为空的过滤器使用

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Article
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','content','pub_time')
+    list_filter = ('pub_time',)
+
+admin.site.register(Article,ArticleAdmin)
