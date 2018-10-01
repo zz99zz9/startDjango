c933c20e66d5fefed65cdf8315d6005bfe08a207
Gavin <4659489@qq.com>
2018-10-01 20:19:29 +0800

    添加文章

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ app_name='blog'
 urlpatterns = [
     url('^$',views.index),
     url('^article/(?P<article_id>[0-9]+)$',views.article_page,name='article_page'),
+    url('^edit/$',views.edit_page,name='edit_page'),
+    url('^edit/action/$',views.edit_action,name='edit_action'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -13,3 +13,13 @@ def index(request):
 def article_page(request,article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request,'article_page.html',{'article':article})
+
+def edit_page(request):
+    return render(request,'edit_page.html')
+
+def edit_action(request):
+    title = request.POST.get('title','TITLE')
+    content = request.POST.get('content','CONTENT')
+    models.Article.objects.create(title=title,content=content)
+    articles = models.Article.objects.all()
+    return render(request,'index.html',{'articles':articles})
